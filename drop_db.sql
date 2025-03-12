61cd8faef6429ff23f647bcff8b942b265959bdc
MildredArquiza <arquizamildred_bsit@plmun.edu.ph>
2025-03-12 18:11:22 +0800

    Delete drop_db.sql
    delete messages

--- drop_db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Students ( id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), email VARCHAR(100) );
-USE `university_db/`;
-SELECT DATABASE();
-SELECT * FROM `students`
-USE `university_db/`;
-DROP DATABASE IF EXISTS `university_db/`;
